b59c9964885ada07ed5e64f6be73d7a08e09c82a
benzthodsapon <benz_thodsapon@outlook.com>
2022-04-22 20:59:29 +0700

    improve jwt

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+var jwt = require("jsonwebtoken");
 
-const posts = [
-  {
-    username: "jan",
-    title: "Test Jan!"
-  },
-  {
-    username: "Benz",
-    title: "Test Benz!"
-  },
-];
+app.get("/api/login", (req, res) => {
+  const user = { id: 3 };
+  const token = jwt.sign({ user }, "my_secret_key");
+  res.json({
+    token: token,
+  });
+});
 
-app.get("/posts",(req, res) => {
-  res.json(posts);
-})
+app.get("/api/protected", ensureToken, (req, res) => {
+  jwt.verify(req.token, "my_secret_key", function (err, data) {
+    if (err) {
+      res.sendStatus(403);
+    } else
+      [
+        res.json({
+          text: "This is login success.",
+          data: data,
+        }),
+      ];
+  });
+});
+
+function ensureToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
-app.listen(port);
+app.listen(port, function () {
+  console.log("App listen on port 3000");
+});
