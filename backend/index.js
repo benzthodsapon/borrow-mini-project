3ba63ab5adea3f64f483b64149c5ca8a7a3d84de
benzthodsapon <benz_thodsapon@outlook.com>
2022-04-22 21:17:00 +0700

    [improve] change structure project

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,12 @@
 const express = require("express");
+
 const app = express();
-const port = 3001;
-var jwt = require("jsonwebtoken");
 
-app.get("/api/login", (req, res) => {
-  const user = { id: 3 };
-  const token = jwt.sign({ user }, "my_secret_key");
-  res.json({
-    token: token,
-  });
-});
+app.use(express.json());
 
-app.get("/api/protected", ensureToken, (req, res) => {
-  jwt.verify(req.token, "my_secret_key", function (err, data) {
-    if (err) {
-      res.sendStatus(403);
-    } else
-      [
-        res.json({
-          text: "This is login success.",
-          data: data,
-        }),
-      ];
-  });
-});
+// authentication
+app.use("/auth", require("./routes/auth"));
 
-function ensureToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
-  if (typeof bearerHeader !== "undefined") {
-    const bearer = bearerHeader.split(" ");
-    const bearerToken = bearer[1];
-    req.token = bearerToken;
-    next();
-  } else {
-    res.sendStatus(403);
-  }
-}
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
-app.listen(port, function () {
-  console.log("App listen on port 3000");
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
 });
